3e891737d26fa6bbbf446119b4fede2173cd1116
Sanjay Makwana <63788069+sunnymac@users.noreply.github.com>
2022-06-07 11:03:31 +0530

    task1 complete

--- lib/main.dart
+++ lib/main.dart
@@ -20,20 +20,42 @@ class MyApp extends StatelessWidget {
         title: Text("My Flutter App"),
         
       ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.network("https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-          width: 200,),
+      body: Container(
+        //color: Colors.amber,
+        height: 100,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10 ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.network("https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=428&q=80",
+              width: 200,),
 
-          Text("The Person",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold
-          ),)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Pizza Abc",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  Text("Price 10",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  Text("Add to Cart",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              )
 
-        ],
+            ],
+          ),
+        ),
       ),
       ),
     );
